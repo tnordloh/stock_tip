9d7866bba05dbe8a9c1a2d0538ac6faa4d4675ab
Timothy Nordloh <tnordloh@gmail.com>
2014-12-09 19:28:14 -0700

    initial commit

--- spec/stock_info_spec.rb
+++ spec/stock_info_spec.rb
@@ -2,6 +2,11 @@ require "minitest/autorun"
 
 require_relative "../lib/stock_info"
 
-describe "StockTip::StockInfo"
+describe "StockTip::StockInfo" do
+  
+  it "gets the price of a stock" do
+    stock_info = StockTip::StockInfo.new()
+    price = stockinfo.price("MCD").must_be_within_delta(100,45)
+  end
 
-
+end
