419abd9d1cfb65db7f7350d613650eb4b337f91f
Timothy Nordloh <tnordloh@gmail.com>
2014-12-09 21:33:34 -0700

    adding yaml reader/writer for account info

---
+++ lib/stock_tip/account_info.rb
@@ -0,0 +1,31 @@
+module StockTip
+  class AccountInfo
+    
+    require 'yaml'
+
+    ACCOUNT_FILE="account.yaml"
+    def initialize(directory)
+      @config_file = "#{directory}/#{ACCOUNT_FILE}"
+      @account_info = nil
+    end
+
+    attr_reader :config_file
+
+    def exists?
+      File.exists?(@config_file)
+    end
+
+    def read_config_file
+      unless account_exists?
+        raise "file #{config_file} nonexistent.  use create_account to create"
+      end
+      @account_info = YAML.load_file(@config_file)
+    end
+
+    def create_account(account: { :name => "Ameritrade", 
+                                  :buy_fee => 999,
+                                  :sell_fee => 1002 } )
+      File.open(@config_file, "w") {|f| f.write account.to_yaml } 
+    end
+  end
+end
--- spec/account_info_spec.rb
+++ spec/account_info_spec.rb
@@ -1,10 +1,19 @@
 require 'minitest/autorun'
 
-require_relative '../lib/account_info'
+require_relative '../lib/stock_tip/account_info'
 
 describe StockTip::AccountInfo do
   it "checks if your broker information exists already" do
     account = StockTip::AccountInfo.new("./data/empty")
     account.exists?.must_equal(false)
+  end
+  
+  it "creates a new account" do
+    account = StockTip::AccountInfo.new("./data/test")
+    account.create_account(account: { :name => "Ameritrade", 
+                                  :buy_fee => 999,
+                                  :sell_fee => 1002 } )
+    account.exists?.must_equal(true)
+    File.delete(account.config_file)
   end
 end
