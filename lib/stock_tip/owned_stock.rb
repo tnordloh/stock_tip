2d9f10bb75ccf39b91e7f011acbf5e31699334c9
Timothy Nordloh <tnordloh@gmail.com>
2014-12-13 15:18:12 -0700

    commit my self work

--- lib/stock_tip/owned_stock.rb
+++ lib/stock_tip/owned_stock.rb
@@ -1,16 +1,41 @@
 module StockTip
+
+  require 'date'
+
   class OwnedStock
-    def initialize(symbol,price_per_share,shares,broker_fee)
+    def initialize(symbol,price_per_share,shares,broker_fee,purchase_date)
       @symbol          = symbol
-      @price_per_share = (price_per_share * 100).to_i
+      @price_per_share = self.class.dollars_to_cents(price_per_share)
       @shares          = shares
-      @broker_fee      = (broker_fee * 100).to_i
+      @broker_fee      = self.class.dollars_to_cents(broker_fee)
+      @purchase_date   = Date.parse(purchase_date)
     end
 
-    attr_reader :shares, :symbol
+    attr_reader :shares, :symbol, :purchase_date
 
-    def cents_to_dollars(cents)
-      cents.to_f / 100
+    class << self
+      def cents_to_dollars(cents)
+        (cents.to_f / 100).round(2)
+      end
+
+      def dollars_to_cents(dollars)
+        (dollars*100).to_i
+      end
     end
+
+    def total_price
+      total = @price_per_share * @shares + @broker_fee
+      self.class.cents_to_dollars(total)
+    end
+
+    def broker_fee
+      self.class.cents_to_dollars(@broker_fee)
+    end
+
+    def sell_value(current_price,broker_fee)
+      current_price = self.class.dollars_to_cents(current_price) 
+      broker_fee = self.class.dollars_to_cents(current_price) 
+    end
+
   end
 end
--- spec/owned_stock_spec.rb
+++ spec/owned_stock_spec.rb
@@ -5,15 +5,20 @@ require_relative '../lib/stock_tip/owned_stock'
 describe StockTip::OwnedStock do
 
   it "can tell me about the stock" do
-    stock = StockTip::OwnedStock.new("MCD",100.00,100,9.99)
+    stock = StockTip::OwnedStock.new("MCD",100.00,100,9.99,"2014-9-9")
     stock.symbol.must_equal("MCD")
     stock.total_price.must_equal(10_009.99)
     stock.shares.must_equal(100)
-    stock.broker_fee.must_equal(100)
+    stock.broker_fee.must_equal(9.99)
+    stock.purchase_date.month.must_equal(9)
   end
 
   it "can convert a cent value to the relevant dollar value" do
-    stock = StockTip::OwnedStock.new("MCD",100.00,100,9.99)
-    stock.cents_to_dollars(100).must_be_within_delta(1.00,0.005)
+    StockTip::OwnedStock.cents_to_dollars(100).must_equal(1.00)
   end
+
+  it "can convert a dollar value to the relevant cent value" do
+    StockTip::OwnedStock.dollars_to_cents(1.00).must_equal(100)
+  end
+
 end
