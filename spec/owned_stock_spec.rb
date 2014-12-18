5c9695bf9f8c78c69a2c9c8fb6751926fe177ddb
Timothy Nordloh <tnordloh@gmail.com>
2014-12-17 17:29:35 -0700

    fix owned_stock tests

--- spec/owned_stock_spec.rb
+++ spec/owned_stock_spec.rb
@@ -1,10 +1,10 @@
 require 'minitest/autorun'
 
 require_relative '../lib/stock_tip/owned_stock'
+require_relative '../lib/stock_tip/stock_info'
 
 describe StockTip::OwnedStock do
-  calculator = lambda  { |symbol| 99_99 }
-  let(:stock) { StockTip::OwnedStock.new("MCD",100.00,100,9.99,"2014-9-9",calculator) }
+  let(:stock) { StockTip::OwnedStock.new("MCD",100.00,100,9.99,"2014-9-9") }
 
   it "can tell me about the stock" do
     stock.symbol.must_equal("MCD")
@@ -17,22 +17,24 @@ describe StockTip::OwnedStock do
   it "can sum up multiple stocks" do
     stocks= [] 
     stocks << stock
-    stocks << StockTip::OwnedStock.new("GE",100.00,100,9.99,"2014-9-9",calculator)
+    stocks << StockTip::OwnedStock.new("GE",100.00,100,9.99,"2014-9-9")
     StockTip::OwnedStock.sum(*stocks).must_equal(20_019_98)
     StockTip::OwnedStock.sum(*stocks, this_method: :broker_fee).must_equal(1998)
   end
 
   it "can get a stock's current sell value" do
-    sell_value = 99_99 * stock.shares - stock.broker_fee
-    stock.sell_value.must_equal(sell_value)
+    stock.sell_value.must_be_within_delta(9_155_01,1_000_00)
   end
 
   it "can get a stock's current value" do
-    stock.current_price.must_equal(99_99) 
+    stock.current_price.must_be_within_delta(91_65,20_00) 
   end
 
-  it "can get a stock's current value" do
-    p stock.to_a
+  it "can get turn a stock into an array" do
+    stock.to_a[0].must_equal("MCD")
+  end
+  it "can get turn a stock into a string" do
+    p stock.to_s.index("MCD").must_equal(0)
   end
   
 end
