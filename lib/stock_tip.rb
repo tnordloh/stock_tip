c640bec400c9d11b0bd47d1af7cac36aca496ec9
Timothy Nordloh <tnordloh@gmail.com>
2014-12-17 17:22:46 -0700

    fix self

--- lib/stock_tip.rb
+++ lib/stock_tip.rb
@@ -1,13 +1,11 @@
 module StockTip
 
-  class << self
-    def cents_to_dollars(cents)
-      (cents.to_f / 100).round(2)
-    end
+  def self.cents_to_dollars(cents)
+    (cents.to_f / 100).round(2)
+  end
 
-    def dollars_to_cents(dollars)
-      (dollars.round(2)*100).to_i
-    end
+  def self.dollars_to_cents(dollars)
+    (dollars.round(2)*100).to_i
   end
 
 end
